0c464f1499f8a43c1316480fecb8f84969b379af
robbert <robbert.vd.zon@alliander.com>
2021-06-16 13:07:45 +0200

    2e pakker in demo SW

--- schaakrob-server/src/main/kotlin/com/vdzon/java/Main.kt
+++ schaakrob-server/src/main/kotlin/com/vdzon/java/Main.kt
@@ -5,7 +5,7 @@ import com.vdzon.java.ui.MainWeb
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        val remote = System.getenv("remote").toLowerCase()
+        val remote = System.getenv()["remote"]?:"false"
         println("remote=$remote")
         val schaakbord = !remote.contains("true")
 //        if (schaakbord) {
